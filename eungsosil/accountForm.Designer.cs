dffb38d05da7a7b2ab7f2f2d46580232f9c070c8
민호 이 <steveleemh@naver.com>
2022-05-08 17:16:37 +0900

    커밋

---
+++ eungsosil/accountForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace eungsosil
+{
+    partial class accountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(accountForm));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(257, 441);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(100, 50);
+            this.axKHOpenAPI1.TabIndex = 0;
+            this.axKHOpenAPI1.UseWaitCursor = true;
+            this.axKHOpenAPI1.OnReceiveTrData += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEventHandler(this.axKHOpenAPI1_OnReceiveTrData);
+            // 
+            // accountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1558, 762);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "accountForm";
+            this.Text = "acccountDlg";
+            this.UseWaitCursor = true;
+            this.Controls.SetChildIndex(this.axKHOpenAPI1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+    }
+}
---
+++ eungsosil/accountForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using System.Runtime.ExceptionServices;
+using System.Security;
+using System.Threading;
+
+namespace eungsosil
+{
+    public partial class accountForm : Form
+
+    {
+
+
+        [HandleProcessCorruptedStateExceptions]
+        [SecurityCritical]
+        public DateTime delay(int MS) //딜레이 매서드
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+
+            while (AfterWards >= ThisMoment)
+            {
+                try
+                {
+                    unsafe
+                    {
+                        System.Windows.Forms.Application.DoEvents();
+                    }
+                }
+                catch (AccessViolationException ex)
+                {
+                    //write_err_log("delay() ex.Message : [" + ex.Message + "]\n", 0);
+                }
+
+                ThisMoment = DateTime.Now;
+            }
+            return DateTime.Now;
+        }
+
+
+        public accountForm()
+        {
+            InitializeComponent();
+        }
+        int g_flag_1 = 0;
+        string g_rqname = null;
+        int g_ord_amt_possible = 0; //총매수가능금액
+
+        int g_scr_no = 0; //open Api 요청번호
+
+        private string get_scr_no()
+        {
+            if (g_scr_no < 9999)
+                g_scr_no++;
+            else
+                g_scr_no = 1000;
+
+            return g_scr_no.ToString();
+        }
+
+        public void set_tb_accnt()
+        {
+            int l_for_cnt = 0;  
+            int l_for_flag = 0;
+
+            g_ord_amt_possible = 0;
+
+            l_for_flag = 0;
+            for (; ; )
+            {
+                axKHOpenAPI1.SetInputValue("계좌번호", axKHOpenAPI1.GetLoginInfo("ACCOUNT_CNT").Trim());
+                axKHOpenAPI1.SetInputValue("비밀번호", "");
+
+                g_rqname = "";
+                g_rqname = "증거금세부내역조회요청";
+                g_flag_1 = 0;
+
+                String l_scr_no = null;
+                l_scr_no = "";
+                l_scr_no = get_scr_no();
+                axKHOpenAPI1.CommRqData("증거금셋부내역조회요청", "opw00013", 0, l_scr_no);
+                l_for_cnt = 0;
+                for(; ; )
+                {
+                    if(g_flag_1 == 1)
+                    {
+                        delay(1000);
+                        axKHOpenAPI1.DisconnectRealData(l_scr_no);
+                        l_for_flag = 1;
+                        break;
+                    }else
+                    {
+                        MessageBox.Show("가져오는 중");
+                        delay(1000);
+                        l_for_cnt++;
+                        if (l_for_cnt == 1)
+                        {
+                            l_for_flag = 0;
+                            break;
+                        }
+                        else
+                            continue;
+                    }
+                }
+                axKHOpenAPI1.DisconnectRealData(l_scr_no);
+                if(l_for_flag==1)
+                {
+                    break;
+                }else if(l_for_flag==0)
+                {
+                    delay(1000);
+                    break;
+                }
+                delay(1000);
+                MessageBox.Show("가져오기 성공\n");
+            }
+
+
+        }
+
+        private void axKHOpenAPI1_OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+
+        }
+    }
+}
