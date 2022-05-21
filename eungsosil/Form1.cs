e6fb7bae2777bbada67b324f1b2cf98a2839efad
cyz065 <parkjh0858@gmail.com>
2022-05-21 12:08:06 +0900

    차트 실시간 수정, list 통합, 차트 & list 예외 처리

--- eungsosil/Form1.cs
+++ eungsosil/Form1.cs
@@ -154,7 +154,7 @@ namespace eungsosil
                 }
 
                 toolStripStatusLabel1.Text = "로그인 완료";
-                tmp_info();
+                stock_info();
                 g_user_id = "";
                 g_user_id = axKHOpenAPI1.GetLoginInfo("USER_ID").Trim();
 
@@ -187,8 +187,25 @@ namespace eungsosil
             toolStripStatusLabel1.Text = "로그아웃 완료";
             txtID.Text = null;
             txtName.Text = null;
+
+            //계좌번호 부분 초기화
+            cmbAcnum1.ResetText();
+            cmbAcnum1.Items.Clear();
+
+            // chart 부분, list 초기화
             stockList.Items.Clear();
-            txtSearch.Text = null;
+            stockList.ResetText();
+
+            stockChart.Series["chart_data"].Points.Clear();
+            stock_name.Text = "";
+            current_price.Text = "";
+            difference.Text = "";
+            start_price.Text = "";
+            low_price.Text = "";
+            high_price.Text = "";
+            total_amount.Text = "";
+            year_high.Text = "";
+            year_low.Text = "";
         }
 
         //증권계좌번호
@@ -543,19 +560,39 @@ namespace eungsosil
             public int end_price { get; set; }
         }
 
-        private void tmp_info()
+        private void stock_info()
         {
-            string l = axKHOpenAPI1.GetCodeListByMarket("0");
-            string[] lst = l.Split(';');
-            for (int i = 0; i < lst.Length; i++)
-                stockList.Items.Add(lst[i]);
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            string code = axKHOpenAPI1.GetCodeListByMarket("0");
+            string[] lst = code.Split(';');
+            string name;
+
+            for (int i = 0; i < lst.Length - 1; i++)
+            {
+                name = axKHOpenAPI1.GetMasterCodeName(lst[i]);
+                dict.Add(name, lst[i]);
+            }
+
+            var key_list = dict.Keys.ToList();
+            key_list.Sort();
+
+            foreach (var key in key_list)
+            {
+                stockList.Items.Add(key + " (" + dict[key] + ")");
+            }
         }
 
         // 차트를 그리기 위한 데이터 요청
         private void requestChart()
         {
+            string code = stockList.SelectedItem.ToString();
+            int start = code.LastIndexOf('(');
+            int end = code.LastIndexOf(')');
+            code = code.Substring(start + 1, end - start - 1);
+
             string date = DateTime.Now.ToString("yyyyMMdd");
-            axKHOpenAPI1.SetInputValue("종목코드", stockList.SelectedItem.ToString().Trim());
+            axKHOpenAPI1.SetInputValue("종목코드", code);
             axKHOpenAPI1.SetInputValue("기준일자", date);
             axKHOpenAPI1.SetInputValue("수정주가구분", "1");
 
@@ -568,7 +605,12 @@ namespace eungsosil
         // 주식 정보요청
         private void requestInfo()
         {
-            axKHOpenAPI1.SetInputValue("종목코드", stockList.SelectedItem.ToString().Trim());
+            string code = stockList.SelectedItem.ToString();
+            int start = code.LastIndexOf('(');
+            int end = code.LastIndexOf(')');
+            code = code.Substring(start + 1, end - start - 1);
+
+            axKHOpenAPI1.SetInputValue("종목코드", code);
             int iRet = axKHOpenAPI1.CommRqData("종목정보요청", "OPT10001", 0, "1003");
 
             if (iRet != 0)
@@ -586,6 +628,7 @@ namespace eungsosil
             // 누적 거래량
             string amount = axKHOpenAPI1.GetCommRealData(e.sRealType, 13).Trim();
             amount = String.Format("{0:n0}", Int32.Parse(amount));
+            total_amount.Text = amount;
 
             // 등락율
             string rate = axKHOpenAPI1.GetCommRealData(e.sRealType, 12).Trim();
@@ -596,6 +639,67 @@ namespace eungsosil
             int d = Int32.Parse(diff);
             diff = String.Format("{0:C}", Math.Abs(d));
 
+            // + / - 에 대하여 텍스트 색상 변경
+            if (r == 0.00)
+            {
+                current_price.ForeColor = Color.Gray;
+                difference.ForeColor = Color.Gray;
+            }
+            else if (r > 0)
+            {
+                current_price.ForeColor = Color.Red;
+                difference.ForeColor = Color.Red;
+            }
+            else if (r < 0)
+            {
+                current_price.ForeColor = Color.Blue;
+                difference.ForeColor = Color.Blue;
+            }
+            current_price.Text = cur_price;
+            difference.Text = diff + " | " + rate + "%";
+
+            // 실시간 현재 가격 get
+            int cur_init_price = Math.Abs(Int32.Parse(axKHOpenAPI1.GetCommRealData(e.sRealType, 10).Trim()));
+
+            // 캔들 차트에 실시간 데이터를 반영
+            if (is_Same_CandleData(priceList[0].date))
+            {
+                // 가격의 변동이 있는 경우 반영
+                priceList[0].end_price = cur_init_price;
+                if (priceList[0].high_price < cur_init_price)
+                    priceList[0].high_price = cur_init_price;
+                if (priceList[0].low_price > cur_init_price)
+                    priceList[0].low_price = cur_init_price;
+
+                // 현재 가격과 차트에 표현된 가격을 비교하여 업데이트
+                // 현재가가 고가보다 높아지는 경우 고가 캔들 상단 업데이트
+                if (cur_init_price > stockChart.Series["chart_data"].Points[0].YValues[0])
+                    stockChart.Series["chart_data"].Points[0].YValues[0] = cur_init_price;
+
+                // 현재가가 저가보다 낮아지는 경우 저가 캔들 하단 업데이트
+                if (stockChart.Series["chart_data"].Points[0].YValues[1] > cur_init_price)
+                    stockChart.Series["chart_data"].Points[0].YValues[1] = cur_init_price;
+                stockChart.Series["chart_data"].Points[0].YValues[3] = cur_init_price;
+
+                // 결과를 차트에 새롭게 반영
+                stockChart.Invalidate();
+            }
+            else
+            {
+                double next_time = double.Parse(priceList[0].date) + 100;
+                PriceInfo item = new PriceInfo();
+                item.date = next_time.ToString();
+
+                item.end_price = item.high_price = item.low_price = item.initial_price = cur_init_price;
+                // 신규 캔들의 경우 list 맨 처음에 추가하여 가장 우측에 출력되도록 설정
+                priceList.Insert(0, item);
+
+                stockChart.Series["chart_data"].Points.InsertXY(0, item.date, item.high_price);
+                stockChart.Series["chart_data"].Points[0].YValues[1] = item.low_price;
+                stockChart.Series["chart_data"].Points[0].YValues[2] = item.initial_price;
+                stockChart.Series["chart_data"].Points[0].YValues[3] = item.end_price;
+                stockChart.Invalidate();
+            }
 
             // 상한가
             string max = high_price_label.Text;
@@ -640,69 +744,6 @@ namespace eungsosil
                 year_min = Convert.ToString(Math.Abs(c));
                 year_low.Text = String.Format("{0:C}", year_min);
             }
-
-            // + / - 에 대하여 텍스트 색상 변경
-            if (r == 0.00)
-            {
-                current_price.ForeColor = Color.Gray;
-                difference.ForeColor = Color.Gray;
-            }
-            else if (r > 0)
-            {
-                current_price.ForeColor = Color.Red;
-                difference.ForeColor = Color.Red;
-            }
-            else if (r < 0)
-            {
-                current_price.ForeColor = Color.Blue;
-                difference.ForeColor = Color.Blue;
-            }
-            current_price.Text = cur_price;
-            difference.Text = diff + " | " + rate + "%";
-            total_amount.Text = amount;
-
-            // 실시간 현재 가격 get
-            int cur_init_price = Math.Abs(Int32.Parse(axKHOpenAPI1.GetCommRealData(e.sRealType, 10).Trim()));
-
-            // 캔들 차트에 실시간 데이터를 반영
-            if (is_Same_CandleData(priceList[0].date))
-            {
-                // 가격의 변동이 있는 경우 반영
-                priceList[0].end_price = cur_init_price;
-                if (priceList[0].high_price < cur_init_price)
-                    priceList[0].high_price = cur_init_price;
-                if (priceList[0].low_price > cur_init_price)
-                    priceList[0].low_price = cur_init_price;
-
-                // 현재 가격과 차트에 표현된 가격을 비교하여 업데이트
-                // 현재가가 고가보다 높아지는 경우 고가 캔들 상단 업데이트
-                if (cur_init_price > stockChart.Series["chart_data"].Points[0].YValues[0])
-                    stockChart.Series["chart_data"].Points[0].YValues[0] = cur_init_price;
-
-                // 현재가가 저가보다 낮아지는 경우 저가 캔들 하단 업데이트
-                if (stockChart.Series["chart_data"].Points[0].YValues[1] > cur_init_price)
-                    stockChart.Series["chart_data"].Points[0].YValues[1] = cur_init_price;
-                stockChart.Series["chart_data"].Points[0].YValues[3] = cur_init_price;
-
-                // 결과를 차트에 새롭게 반영
-                stockChart.Invalidate();
-            }
-            else
-            {
-                double next_time = double.Parse(priceList[0].date) + 100;
-                PriceInfo item = new PriceInfo();
-                item.date = next_time.ToString();
-
-                item.end_price = item.high_price = item.low_price = item.initial_price = cur_init_price;
-                // 신규 캔들의 경우 list 맨 처음에 추가하여 가장 우측에 출력되도록 설정
-                priceList.Insert(0, item);
-
-                stockChart.Series["chart_data"].Points.InsertXY(0, item.date, item.high_price);
-                stockChart.Series["chart_data"].Points[0].YValues[1] = item.low_price;
-                stockChart.Series["chart_data"].Points[0].YValues[2] = item.initial_price;
-                stockChart.Series["chart_data"].Points[0].YValues[3] = item.end_price;
-                stockChart.Invalidate();
-            }
         }
 
         private void axKHOpenAPI1_OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
@@ -710,8 +751,8 @@ namespace eungsosil
             // 한번 data 받아서 data 출력
             if (e.sRQName == "종목정보요청")
             {
-                
-                string code= axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "종목코드").Trim();
+
+                string code = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "종목코드").Trim();
 
                 string name = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "종목명").Trim();
                 string cur_price = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "현재가").Trim();
@@ -830,6 +871,18 @@ namespace eungsosil
 
         private void searchBtn_Click(object sender, EventArgs e)
         {
+            // 예외 처리
+            if (stockList.Items.Count == 0)
+            {
+                MessageBox.Show("로그인 후 이용 가능합니다", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (stockList.SelectedItem == null)
+            {
+                MessageBox.Show("검색 종목을 선택해 주세요", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             stockChart.Series["chart_data"].Points.Clear();
             priceList = new List<PriceInfo>();
             requestInfo();
@@ -950,7 +1003,7 @@ namespace eungsosil
 
 
 
-
+        /*
         //종목명으로 종목정보,차트검색하기
         public void stockSearch(object sender, EventArgs e)
         {
@@ -988,14 +1041,14 @@ namespace eungsosil
                         MessageBox.Show("차트요청 실패");
 
 
-                    /*axKHOpenAPI1.SetInputValue("종목코드", 종목코드[i]);
-                    axKHOpenAPI1.CommRqData("종목정보요청", "opt10001", 0, "5000");*/
+                    //axKHOpenAPI1.SetInputValue("종목코드", 종목코드[i]);
+                    //axKHOpenAPI1.CommRqData("종목정보요청", "opt10001", 0, "5000");
                 }
 
                
             }
 
-        }
+        }*/
 
 
         private void cmbAcnum1_SelectedIndexChanged_1(object sender, EventArgs e)
